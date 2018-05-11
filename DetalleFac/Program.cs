0dc84b76a58799aae391327213c19b66002469f1
Xavier Morales <jjmm7@live.com>
2018-05-11 02:04:22 -0500

    Primera Subida

---
+++ DetalleFac/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetalleFac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float valor1, valor2, valor3, valor4;
+            Console.WriteLine("FACTURA");
+            Console.WriteLine("*******************************");
+            Console.WriteLine("Ingrese Valor Producto 1 ");valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Valor Producto 2 "); valor2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Valor Producto 3 "); valor3 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Valor Producto 4 "); valor4 = float.Parse(Console.ReadLine());
+            Console.WriteLine("IMPRIMIENDO VALORES ADICIONALES");
+            float sumVal = valor1 + valor2 + valor3 + valor4;
+            Console.WriteLine("SUBTOTAL : " + sumVal);
+            float iva = (sumVal) * (0.12); 
+            Console.WriteLine("IVA : " +iva);
+            Console.WriteLine("TOTAL : " + (sumVal +iva));
+
+        }
+    }
+}
---
+++ Sueldo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sueldo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            float salario, valorHora;
+            
+            //5*8 = 40 *4 = 160
+            
+            Console.WriteLine("CALCULAR VALOR HORA");
+            Console.WriteLine("********************");
+            Console.WriteLine("Ingrese Salario: ");
+            Console.WriteLine("******************");
+            salario = float.Parse(Console.ReadLine());
+            valorHora = salario / 160;
+            Console.WriteLine("Costo calculado por hora: $" + valorHora);
+
+            Console.ReadLine();
+
+        }
+    }
+}
